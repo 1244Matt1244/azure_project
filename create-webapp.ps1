b447d3434f039c4483b22b60fb71e9197011a032
44Matt44 <matt123.3a@gmail.com>
2024-05-27 16:54:26 +0200

    Update create-webapp.ps1

--- create-webapp.ps1
+++ create-webapp.ps1
@@ -1,1 +1,22 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$resourceGroupName,
+    
+    [Parameter(Mandatory=$true)]
+    [string]$servicePlanName,
+    
+    [Parameter(Mandatory=$true)]
+    [string]$webAppName,
+    
+    [Parameter(Mandatory=$true)]
+    [string]$location
+)
 
+# Connect to Azure account
+Connect-AzAccount
+
+# Create the Web App
+$webApp = New-AzWebApp -ResourceGroupName $resourceGroupName -Name $webAppName -Location $location -AppServicePlan $servicePlanName
+
+# Output the Web App details
+Write-Output "Web App created: $($webApp.DefaultHostName)"
